bb23836f89bc284e452c84cd246bd88813fe54bb
Artilonhyseni <146942529+Artilonhyseni@users.noreply.github.com>
2024-06-04 23:25:40 +0200

    det skulle gerne virke nu når vi har sections på

--- togglejs.js
+++ togglejs.js
@@ -1,3 +1,19 @@
 // JavaScript Document
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.menu a');
 
-console.log("hello");
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
