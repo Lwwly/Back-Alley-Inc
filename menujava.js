2079bc78e597ad6dda3c662624d6cab9cc8e4e26
Artilonhyseni <146942529+Artilonhyseni@users.noreply.github.com>
2024-06-13 23:59:50 +0200

    små tilføjelser

--- menujava.js
+++ menujava.js
@@ -2,6 +2,8 @@
 
 // script.js
 
+// for const links links.addeventlistner samler alle <a> under <li> //
+
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
 
@@ -11,9 +13,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function smoothScroll(e) {
         e.preventDefault();
+		
+		// smooth scroller funktionen // 
+		
+		
+		// her bliver selve id*en targeted 
 
         const targetId = this.getAttribute('href');
         const targetSection = document.querySelector(targetId);
+		
+		// selve rulleren // 
 
         window.scrollTo({
             top: targetSection.offsetTop,
